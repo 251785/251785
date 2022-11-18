348beb6793aded3d0195eace1d27bd88af5d946a
Karhik Peravali <you@example.com>
2022-11-18 12:10:24 -0600

    Original M2222 Recursion

---
+++ C101 Factorial [Swift]/main.swift
@@ -0,0 +1,30 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+
+
+func factorial(from  n: Int) -> Int {
+    print("Calculating factorial(\(n))...")
+    // recursive call
+    if n > 1 {
+        let x = n * factorial(from:  n - 1)
+        print("Non-terminal case, returning \(x).")
+        return x
+        }
+    // condition to break recursion
+    else {
+        print("Terminal case, returning 1.")
+        return 1
+    }           
+}
+
+let result = factorial(from:  n)
+print("The final result is: \(result)).")
+
---
+++ C102 Fibonacci [Swift]/main.swift
@@ -0,0 +1,28 @@
+
+
+// ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+let n = 7
+// DO NOT ALTER THE TEXT BETWEEN THESE LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================
+// ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧  ⇧
+
+
+// Add your code below:
+let a = 21
+func fib( int: Int) -> Int {
+    print("Calculating fibonacci(\(int))...")
+    if(int <= 2){
+        
+        print("Terminal case, returning 1.")
+        return 1
+    }
+    else{
+        
+        let x = fib(int: int-1) + fib(int: int-2)
+        print("Non-terminal case, returning \(x).")
+        return x
+    }
+}
+
+let result = fib(int:  n)
+print("The final result is: \(result)).")
